b77c36f0ee638231832110085d959b7efb6dc6e6
Rohan Rajput <the.ronic.30@gmail.com>
2024-12-01 04:07:04 +0530

    js

---
+++ js/Q2/script.js
@@ -0,0 +1,12 @@
+
+function result() {
+    let num = document.getElementById("num").value;
+    var diff = 13 - num;
+
+    if (num < 13) {
+        alert(diff);
+    }
+    else{
+        alert(diff * 2);
+    }
+}

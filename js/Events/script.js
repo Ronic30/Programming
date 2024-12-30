e12cc11451f38d289fa62c4b3aa6495db4ba68cf
Rohan Rajput <the.ronic.30@gmail.com>
2024-12-30 22:52:03 +0530

    js

---
+++ js/Events/script.js
@@ -0,0 +1,5 @@
+let button = document.getElementById(".btn")
+
+button.addEventListener("dblclick", ()=>{
+    document.querySelector(".box").innerHTML = "<b>Yayy you were clicked</b> Enjoy your click!"
+})

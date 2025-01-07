320819f89d7f81e2cc476d7c0bb5e1e5622c2c91
Rohan Rajput <the.ronic.30@gmail.com>
2025-01-07 16:44:08 +0530

    js

--- js/DOM/Part2/script.js
+++ js/DOM/Part2/script.js
@@ -1,9 +1,9 @@
-// let box = document.getElementsByClassName("box");
-// console.log(box);
+let box = document.getElementsByClassName("box");
+console.log(box);
 
-// box[1].style.backgroundColor = "green";
+box[1].style.backgroundColor = "green";
 
-// document.getElementById("fix").style.backgroundColor = "blue";
+document.getElementById("fix").style.backgroundColor = "blue";
 
 document.querySelector(".box").style.backgroudColor = "red";
 console.log (document.querySelectorAll(".box"))

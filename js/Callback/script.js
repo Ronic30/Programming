ba0f321bab17fd5a2f168adef2cf900ba3d3389d
Rohan Rajput <the.ronic.30@gmail.com>
2025-01-09 07:38:12 +0530

    js

---
+++ js/Callback/script.js
@@ -0,0 +1,17 @@
+const fn = () => {
+    console.log("Nothing")
+}
+
+const callback = (arg, fn) => {
+    console.log(arg)
+    fn()
+}
+
+function loadScript(src, callback){
+    let sc = document.createElement("script")
+    sc.src = src
+    sc.onload = () => callback("Rohan", fn)
+    document.head.append(sc)
+}
+
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", callback)
